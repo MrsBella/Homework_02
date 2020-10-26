aaa3ea43d2bd3b3a3d8a29b947fbf35d3ec72123
Izabela Garbacz <izabelagarbacz@vp.pl>
2020-10-26 14:51:22 +0100

    Zadanie 1

---
+++ src/main/java/pl/coderslab/controller/HeaderController.java
@@ -0,0 +1,17 @@
+package pl.coderslab.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+@Controller
+public class HeaderController {
+
+    @GetMapping("/showUserAgent")
+    public String userAgent(@RequestHeader("user-agent") String userAgent, Model model) {
+        model.addAttribute("userAgent", userAgent);
+
+        return "userAgent";
+    }
+}
