c95138714180f8b5d91b4a4629100df9352643cd
Izabela Garbacz <izabelagarbacz@vp.pl>
2020-10-29 10:10:22 +0100

    Zadanie 3

--- src/main/java/pl/coderslab/controller/HelloController.java
+++ src/main/java/pl/coderslab/controller/HelloController.java
@@ -1,39 +1,33 @@
 package pl.coderslab.controller;
 
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.Scanner;
+import java.io.*;
+import java.util.Arrays;
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.Collectors;
 
 @Controller
 public class HelloController {
 
     @GetMapping("/workers")
-    public String workerAction(Model model) throws FileNotFoundException {
+    public String workerAction(Model model) throws IOException {
 
         int randomNumber = ThreadLocalRandom.current().nextInt(1, 30);
 
-        File file = new File("Workers.txt");
-        System.out.println(file.exists());
-        try {
-            Scanner scanner = new Scanner(file);
-            while (scanner.hasNextLine()) {
-                System.out.println(scanner.next());
-//                String[] workers = scanner.nextLine().split(",");
-//                int number = Integer.parseInt(workers[0]);
-//                if (randomNumber == number) {
-//                    model.addAttribute("worker", workers[1]);
-                }
-        } catch (FileNotFoundException e) {
-            return "brakPliku";
-        }
+        InputStream inputStream = new ClassPathResource("Workers.txt").getInputStream();
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
+            String workers = reader.lines().collect(Collectors.joining("\n"));
+            String[] array = workers.split("\n");
+            System.out.println(Arrays.asList(array));
+            for (int i = 0; i < array.length; i++) {
+                String[] worker = array[i].split(",");
 
+            }
+        }
         return "workers";
     }
 }
